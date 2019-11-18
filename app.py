a47a251f47b187cbc11a9c2e704c40caedd88bc4
myname21-meet <myname21@meet.mit.edu>
2019-11-18 12:57:49 -0500

    LAB 3 DONE

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, redirect, url_for, render_template
+from flask import session as login_session
+
+app = Flask(__name__)
+app.secret_key = "MY_SUPER_SECRET_KEY"
+
+
+##### Code here ######
+@app.route('/')
+def home():
+	return render_template("home.html")
+
+@app.route('/store')
+def store():
+	return render_template('store.html')
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/cart')
+def cart():
+	return render_template('cart.html')
+
+@app.route('/cart/<name>')
+def cart1(name):
+	
+	return render_template('store.html', product = name)
+
+
+
+
+
+
+
+#####################
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
